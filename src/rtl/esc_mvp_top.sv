9b89bb13962dacc978bba1a05cb9a1c6842f1a58
merkelmarrow <69924036+merkelmarrow@users.noreply.github.com>
2025-08-16 17:45:00 +0100

    feat: new mvp top module

---
+++ src/rtl/esc_mvp_top.sv
@@ -0,0 +1,31 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: N/A
+// Engineer: Marco Blackwell
+// 
+// Create Date: 16.08.2025 17:34:51
+// Design Name: 
+// Module Name: esc_mvp_top
+// Project Name: PYNQ ESC
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module esc_mvp_top(
+    input clk_125_in,
+    input rst_n,
+    output adc_mclk_out,
+    output pwm_out
+    );
+    
+    
+endmodule
